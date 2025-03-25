5783d539042432e43de4f731e9ed779699d5ac4b
srushti15km <srushtikm04@gmail.com>
2025-03-25 12:17:10 +0530

    yash.java

--- yash.java
+++ yash.java
@@ -1,4 +1,4 @@
-public class yash
-{public static void main(String args[]){
-{System.out.println("hello");
+public class yash{
+public static void main(String args[]){
+System.out.println("hello");
 }}
